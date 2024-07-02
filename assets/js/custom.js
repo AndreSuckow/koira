6f034e9abf0195b0e788f28c4c7ee7cf74774236
André Luis Suckow <alskw0911@gmail.com>
2024-07-02 18:53:34 -0300

    ultimo update

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const whatsappButton = document.getElementById('whatsapp-button');
+//     let buttonActivated = false;
+  
+//     window.addEventListener('scroll', function() {
+//       if (window.scrollY > 700 && !buttonActivated) {
+//         whatsappButton.classList.add('visible');
+//         buttonActivated = true; // Marcar o botão como ativado
+//       }
+//     });
+//   });
+  
